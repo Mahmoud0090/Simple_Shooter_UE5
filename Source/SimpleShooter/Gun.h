b11689262d1868358cf927e6959517955a8bff2d
Mahmoud Abdulaal <mahmoud.abdulaal@intaktiv.intl>
2022-09-23 10:35:01 +0300

    Shooting

--- Source/SimpleShooter/Gun.h
+++ Source/SimpleShooter/Gun.h
@@ -14,6 +14,7 @@ class SIMPLESHOOTER_API AGun : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AGun();
+	void PullTrigger();
 
 protected:
 	// Called when the game starts or when spawned
@@ -29,5 +30,8 @@ private:
 	
 	UPROPERTY(VisibleAnyWhere)
 	USkeletalMeshComponent* Mesh; 
+
+	UPROPERTY(EditAnyWhere)
+	UParticleSystem* MuzzleFlash;
 
 };
